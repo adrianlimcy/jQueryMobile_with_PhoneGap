38f9b3527fad0ac58a57680d4f091f42ce439246
adrianlimcy <adrianlimcy@gmail.com>
2018-05-24 11:33:22 +0800

    Chapter 5 completed

--- platforms/browser/www/js/custom-jqm-transitions.js
+++ platforms/browser/www/js/custom-jqm-transitions.js
@@ -1,3 +1,5 @@
 $(document).bind("mobileinit", function(){
   $.mobile.defaultPageTransition = 'fade';
+  $.mobile.loadingMessage = 'Please Wait';
+  $.mobile.pageLoadErrorMessage = 'There was an error, please try again';
 });
