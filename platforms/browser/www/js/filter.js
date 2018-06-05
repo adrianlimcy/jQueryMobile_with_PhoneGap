64794fcecb6ef976c347faf825570df7a37b6677
adrianlimcy <adrianlimcy@gmail.com>
2018-06-05 12:46:24 +0800

    Chapter 9 done ... ish ... a lot of deprecated stuff

---
+++ platforms/browser/www/js/filter.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+  // $('#beatles-page').on("pagebeforecreate", function(event) {
+  //   $('#beatles-listview').listview('option', 'filterCallback', defaultSearch);
+  //   console.log("page created");
+  //   alert('test');
+  // });
+  // $("#beatles-page").on("pagecontainerload", function(event, ui) {
+
+  // $(document).on("pagecreate", function(event, ui) {
+  //   // alert("Test");
+  //   console.log("page created");
+  // });
+// $(function(){
+  $(document).on("pagecontainercreate", function(event, ui) {
+    console.log("page loaded");
+  });
+// });
+// });
+function defaultSearch( text, searchValue) {
+  console.log("Text: " + text, ", SearchValue: " + searchValue);
+  return text.toLowerCase().indexOf(searchValue) ===  -1;
+};
+
+// as of 1.4, filterCallback is deprecated
